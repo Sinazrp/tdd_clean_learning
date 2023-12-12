511455bfe847c7491189f0e310e16b5d9922fac2
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-12 23:15:31 +0330

    Create get_user.dart

---
+++ lib/features/auth/domain/usecases/get_user.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:tdd_clean_learning/core/errors/failure.dart';
+import 'package:tdd_clean_learning/core/usecase/usecase.dart';
+import 'package:tdd_clean_learning/core/utils/typedef.dart';
+import 'package:tdd_clean_learning/features/auth/domain/entities/user.dart';
+import 'package:tdd_clean_learning/features/auth/domain/repositories/auth_repo.dart';
+
+class GetUser extends UseCaseVoid<Either<Failure, List<User>>> {
+  final AuthRepository _authRepository;
+  GetUser(this._authRepository);
+  @override
+  ResultFuture<List<User>> call() async {
+    return await _authRepository.getUsers();
+  }
+}
