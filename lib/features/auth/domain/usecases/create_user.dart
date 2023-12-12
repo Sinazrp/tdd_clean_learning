5816458ff6529d5cfbb705ed44c9888c76c65df9
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-12 21:46:02 +0330

    Update create_user.dart

--- lib/features/auth/domain/usecases/create_user.dart
+++ lib/features/auth/domain/usecases/create_user.dart
@@ -1,21 +1,29 @@
+import 'package:equatable/equatable.dart';
+
 import 'package:tdd_clean_learning/core/usecase/usecase.dart';
 import 'package:tdd_clean_learning/core/utils/typedef.dart';
 
 import '../repositories/auth_repo.dart';
 
-class CreateUSer extends UseCase<void, dynamic> {
+class CreateUser extends UseCase<void, CreateUserParams> {
   final AuthRepository _authRepository;
-
-  const CreateUSer(this._authRepository);
-  ResultFuture<void> createUser(
-          {required String createdAt,
-          required String name,
-          required String avatar}) async =>
-      _authRepository.createUser(
-          createdAt: createdAt, name: name, avatar: avatar);
+  CreateUser(this._authRepository);
 
   @override
-  ResultFuture call(param) {
-    throw UnimplementedError();
+  Future<void> call(CreateUserParams params) {
+    return _authRepository.createUser(
+        createdAt: params.createdAt, name: params.name, avatar: params.avatar);
   }
+}
+
+class CreateUserParams extends Equatable {
+  final String createdAt;
+  final String name;
+  final String avatar;
+
+  const CreateUserParams(
+      {required this.createdAt, required this.name, required this.avatar});
+
+  @override
+  List<Object> get props => [createdAt, name, avatar];
 }
