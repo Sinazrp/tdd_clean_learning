a730d41517a541824bcd840f9a23381b6d6affa2
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-12 20:05:44 +0330

    Update auth_repo.dart

--- lib/features/auth/domain/repositories/auth_repo.dart
+++ lib/features/auth/domain/repositories/auth_repo.dart
@@ -1,12 +1,11 @@
-import 'package:dartz/dartz.dart';
-import 'package:tdd_clean_learning/core/errors/failure.dart';
+import 'package:tdd_clean_learning/core/utils/typedef.dart';
 import 'package:tdd_clean_learning/features/auth/domain/entities/user.dart';
 
 abstract class AuthRepository {
   const AuthRepository();
-  Future<Either<Failure, void>> createUser(
+  ResultFuture<void> createUser(
       {required String createdAt,
       required String name,
       required String avatar});
-  Future<Either<Failure, List<User>>> getUsers();
+  ResultFuture<List<User>> getUsers();
 }
