15b5d7a8fbabca3d01a1c6b54142e40ac0777f0d
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-14 19:56:19 +0330

    Update create_user_test.dart

--- test/features/auth/domain/usecases/create_user_test.dart
+++ test/features/auth/domain/usecases/create_user_test.dart
@@ -1,7 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_clean_learning/features/auth/domain/repositories/auth_repo.dart';
+import 'package:tdd_clean_learning/features/auth/domain/usecases/create_user.dart';
+
+class MockAuthRepo extends Mock implements AuthRepository {}
 
 void main() {
-  testWidgets('create user ...', (tester) async {
-    // TODO: Implement test
+  late CreateUser useCase;
+  late AuthRepository repository;
+  setUp(() {
+    repository = MockAuthRepo();
+    useCase = CreateUser(repository);
+  });
+  test('should call the [AuthRepo.createUser]', () async {
+    // Arrangement
+    const params = CreateUserParams(
+        avatar: 'avatar-1.jpg', createdAt: 'created at now', name: 'hilarooda');
+
+    //act
+    useCase(params);
+    //assert
   });
 }
