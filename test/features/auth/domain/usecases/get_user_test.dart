a01ee4867789cafeafccc56bfcdeee58db03abff
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-15 20:15:36 +0330

    implemented get user test

--- test/features/auth/domain/usecases/get_user_test.dart
+++ test/features/auth/domain/usecases/get_user_test.dart
@@ -1,5 +1,8 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:tdd_clean_learning/core/errors/failure.dart';
+import 'package:tdd_clean_learning/features/auth/domain/entities/user.dart';
 import 'package:tdd_clean_learning/features/auth/domain/repositories/auth_repo.dart';
 import 'package:tdd_clean_learning/features/auth/domain/usecases/get_user.dart';
 
@@ -8,16 +11,21 @@ import 'usecase_mocks.dart';
 void main() {
   late final GetUser useCase;
   late final AuthRepository repository;
+  const user = User.empty();
   setUp(() {
     repository = MockAuthRepo();
     useCase = GetUser(repository);
   });
-  test('it should call [Repository.getUser()]', () {
+  test('it should call [Repository.getUser()]', () async {
     //Arrange
-    when(() => repository.getUsers()).;
+    when(() => repository.getUsers())
+        .thenAnswer((_) async => const Right<Failure, List<User>>([user]));
 
     //act
-
+    final result = await useCase();
     // Assert
+    expect(result, equals(const Right<Failure, List<User>>([user])));
+    verify(() => repository.getUsers()).called(1);
+    verifyNoMoreInteractions(repository);
   });
 }
