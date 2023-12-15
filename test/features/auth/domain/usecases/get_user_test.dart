6b8e45295708691b0996119ae4d9af115fdaaa83
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-15 16:01:49 +0330

    Update get_user_test.dart

--- test/features/auth/domain/usecases/get_user_test.dart
+++ test/features/auth/domain/usecases/get_user_test.dart
@@ -1,7 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_clean_learning/features/auth/domain/repositories/auth_repo.dart';
+import 'package:tdd_clean_learning/features/auth/domain/usecases/get_user.dart';
+
+import 'usecase_mocks.dart';
 
 void main() {
-  testWidgets('get user ...', (tester) async {
-    // TODO: Implement test
+  late final GetUser useCase;
+  late final AuthRepository repository;
+  setUp(() {
+    repository = MockAuthRepo();
+    useCase = GetUser(repository);
+  });
+  test('it should call [Repository.getUser()]', () {
+    //Arrange
+    when(() => repository.getUsers()).;
+
+    //act
+
+    // Assert
   });
 }
