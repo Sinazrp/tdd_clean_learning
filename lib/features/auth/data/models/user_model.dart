f9ebd077ec4ea926cfef68029c86defd6e2da2b0
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-15 21:29:18 +0330

    Update user_model.dart

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,6 +1,20 @@
+import 'package:tdd_clean_learning/core/utils/typedef.dart';
+
 import '../../domain/entities/user.dart';
 
 class UserModel extends User {
-  const UserModel(int id, String createdAt, String name, String avatar)
+  //const  UserModel({required super.id, required super.createdAt, required super.name, required super.avatar});
+  const UserModel(
+      {required id,
+      required String createdAt,
+      required name,
+      required String avatar})
       : super(id: id, createdAt: createdAt, name: name, avatar: avatar);
+
+  UserModel.fromMap(DataMap map)
+      : this(
+            avatar: map['avatar'],
+            name: map['name'],
+            createdAt: map['createdAt'],
+            id: map['id']);
 }
