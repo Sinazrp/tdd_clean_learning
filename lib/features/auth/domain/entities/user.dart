54fa167f4c759c3f67d83c7e48d09c09bc7e9d55
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-15 20:15:16 +0330

    add empty constructor

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -11,6 +11,12 @@ class User extends Equatable {
       required this.createdAt,
       required this.name,
       required this.avatar});
+  const User.empty()
+      : this(
+            avatar: 'empty.avatar',
+            createdAt: 'empty.createdAt',
+            name: 'empty.name',
+            id: 1);
 
   @override
   List<Object?> get props => [id, createdAt, name, avatar];
