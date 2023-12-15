8bc4cab67fdd707e904e9c091554635514979a56
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-15 22:19:02 +0330

    Update user_model.dart

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,20 +1,33 @@
+import 'dart:convert';
+
 import 'package:tdd_clean_learning/core/utils/typedef.dart';
 
 import '../../domain/entities/user.dart';
 
 class UserModel extends User {
-  //const  UserModel({required super.id, required super.createdAt, required super.name, required super.avatar});
   const UserModel(
-      {required id,
+      {required String id,
       required String createdAt,
-      required name,
+      required String name,
       required String avatar})
       : super(id: id, createdAt: createdAt, name: name, avatar: avatar);
 
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(jsonDecode(source) as DataMap);
+
   UserModel.fromMap(DataMap map)
       : this(
-            avatar: map['avatar'],
-            name: map['name'],
-            createdAt: map['createdAt'],
-            id: map['id']);
+          avatar: map['avatar'] as String,
+          name: map['name'] as String,
+          createdAt: map['createdAt'] as String,
+          id: map['id'] as String,
+        );
+
+  DataMap toMap() => {
+        'id': id,
+        'avatar': avatar,
+        'name': name,
+        'createdAt': createdAt,
+      };
+  String toJson() => jsonEncode(toMap());
 }
