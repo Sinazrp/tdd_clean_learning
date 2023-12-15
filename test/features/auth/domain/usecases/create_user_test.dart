64d924d55bca98f63442c587d8b0520940baf11b
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-15 14:51:31 +0330

    create first test

--- test/features/auth/domain/usecases/create_user_test.dart
+++ test/features/auth/domain/usecases/create_user_test.dart
@@ -1,3 +1,4 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:tdd_clean_learning/features/auth/domain/repositories/auth_repo.dart';
@@ -8,17 +9,30 @@ class MockAuthRepo extends Mock implements AuthRepository {}
 void main() {
   late CreateUser useCase;
   late AuthRepository repository;
+  const params = CreateUserParams.empty();
   setUp(() {
     repository = MockAuthRepo();
     useCase = CreateUser(repository);
   });
   test('should call the [AuthRepo.createUser]', () async {
     // Arrangement
-    const params = CreateUserParams(
-        avatar: 'avatar-1.jpg', createdAt: 'created at now', name: 'hilarooda');
+    when(
+      () => repository.createUser(
+          createdAt: any(named: 'createdAt'),
+          name: any(named: 'name'),
+          avatar: any(named: 'avatar')),
+    ).thenAnswer((_) async => const Right(null));
 
     //act
-    useCase(params);
+    final result = await useCase(params);
+
     //assert
+    expect(result, equals(const Right<dynamic, void>(null)));
+
+    verify(() => repository.createUser(
+        avatar: params.avatar,
+        name: params.name,
+        createdAt: params.createdAt)).called(1);
+    verifyNoMoreInteractions(repository);
   });
 }
