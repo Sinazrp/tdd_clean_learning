93ad7039f1749c369a9f170f53ab4e910ace52cd
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-15 16:01:47 +0330

    Create usecase_mocks.dart

---
+++ test/features/auth/domain/usecases/usecase_mocks.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_clean_learning/features/auth/domain/repositories/auth_repo.dart';
+
+class MockAuthRepo extends Mock implements AuthRepository {}
