3ac48925f6908e428797e73e311622a4516effc6
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-16 19:26:06 +0330

    Update user_model_test.dart

--- test/features/auth/data/models/user_model_test.dart
+++ test/features/auth/data/models/user_model_test.dart
@@ -1,15 +1,34 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_clean_learning/features/auth/data/models/user_model.dart';
 import 'package:tdd_clean_learning/features/auth/domain/entities/user.dart';
 
+import '../../../../fixtures/fixture_reader.dart';
+
 void main() {
+  const tModel = UserModel.empty();
   test('should be subclass of [User] entity ', () {
     //Arrange
-    const tModel = UserModel.empty();
 
     //Act : no action
 
     // assert
     expect(tModel, isA<User>());
+  });
+
+  final tJson = fixture('user.json');
+  final tMap = jsonDecode(tJson);
+  group('from map ', () {
+    test('should return a [UserModel] with the right data ', () {
+      //Arrange
+
+      //act
+      final result = UserModel.fromMap(tMap);
+
+      //assert
+      expect(result, equals(tModel));
+    });
   });
 }
