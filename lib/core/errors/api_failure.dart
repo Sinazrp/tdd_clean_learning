15dd8a1ec7ae91ea56c21b7fdc568539be038977
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-17 20:07:56 +0330

    Update api_failure.dart

--- lib/core/errors/api_failure.dart
+++ lib/core/errors/api_failure.dart
@@ -1,7 +1,10 @@
+import 'package:tdd_clean_learning/core/errors/exceptions.dart';
 import 'package:tdd_clean_learning/core/errors/failure.dart';
 
 /// Represents a failure or error that occurs when making an API call.
 class ApiFailure extends Failure {
   const ApiFailure({required String message, required int statusCode})
       : super(message: message, statusCode: statusCode);
+  ApiFailure.fromE({required ServerException e})
+      : this(message: e.message, statusCode: e.statusCode);
 }
