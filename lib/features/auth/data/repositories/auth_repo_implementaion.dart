c07730a17899d3200e2607d3caa319540d92f270
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-17 18:54:48 +0330

    Update auth_repo_implementaion.dart

--- lib/features/auth/data/repositories/auth_repo_implementaion.dart
+++ lib/features/auth/data/repositories/auth_repo_implementaion.dart
@@ -1,3 +1,5 @@
+import 'package:dartz/dartz.dart';
+import 'package:tdd_clean_learning/core/errors/failure.dart';
 import 'package:tdd_clean_learning/core/utils/typedef.dart';
 import 'package:tdd_clean_learning/features/auth/data/datasources/remote/auth_remote_datasource.dart';
 import 'package:tdd_clean_learning/features/auth/domain/entities/user.dart';
@@ -11,8 +13,10 @@ class AuthRepoImplementation implements AuthRepository {
   ResultFuture<void> createUser(
       {required String createdAt,
       required String name,
-      required String avatar}) {
-    throw UnimplementedError();
+      required String avatar}) async {
+    await _remoteDataSource.createUser(
+        createdAt: createdAt, name: name, avatar: avatar);
+    return const Right<Failure, void>(null);
   }
 
   @override
