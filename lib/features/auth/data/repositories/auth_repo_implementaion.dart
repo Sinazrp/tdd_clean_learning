8b3c9e71c6480dac1ac686a741684d0b8f0f8037
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-17 16:35:28 +0330

    fix some issue

--- lib/features/auth/data/repositories/auth_repo_implementaion.dart
+++ lib/features/auth/data/repositories/auth_repo_implementaion.dart
@@ -1,20 +1,22 @@
 import 'package:tdd_clean_learning/core/utils/typedef.dart';
+import 'package:tdd_clean_learning/features/auth/data/datasources/remote/auth_remote_datasource.dart';
 import 'package:tdd_clean_learning/features/auth/domain/entities/user.dart';
 import 'package:tdd_clean_learning/features/auth/domain/repositories/auth_repo.dart';
 
 class AuthRepoImplementation implements AuthRepository {
+  final AuthRemoteDataSource _remoteDataSource;
+
+  AuthRepoImplementation(this._remoteDataSource);
   @override
   ResultFuture<void> createUser(
       {required String createdAt,
       required String name,
       required String avatar}) {
-    // TODO: implement createUser
     throw UnimplementedError();
   }
 
   @override
   ResultFuture<List<User>> getUsers() {
-    // TODO: implement getUsers
     throw UnimplementedError();
   }
 }
--- test/features/auth/data/models/user_model_test.dart
+++ test/features/auth/data/models/user_model_test.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_clean_learning/features/auth/data/models/user_model.dart';
