31f07c4c81255f0ec676253bc77588546902bb7c
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-18 23:15:28 +0330

    Update auth_remote_datasource_test.dart

--- test/features/auth/data/datasources/remote/auth_remote_datasource_test.dart
+++ test/features/auth/data/datasources/remote/auth_remote_datasource_test.dart
@@ -1,16 +1,32 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
 import 'package:tdd_clean_learning/features/auth/data/datasources/remote/auth_remote_datasource.dart';
+import 'package:tdd_clean_learning/features/auth/data/models/user_model.dart';
 
 class MockHttpClient extends Mock implements http.Client {}
 
 void main() {
+  UserModel tparams = const UserModel.empty();
   late http.Client client;
   late AuthRemoteDataSource remoteDataSource;
   setUp(() {
     client = MockHttpClient();
     remoteDataSource = AuthRemoteDataSourceImple(client);
   });
-  group('createUser', () {});
+  group('createUser', () {
+    test('should complete successfully', () async {
+      when(() => client.post(any(), body: any(named: 'body'))).thenAnswer(
+          (_) async => http.Response('Usser Created succesfully', 201));
+      final methodCall = await remoteDataSource.createUser;
+
+      expect(
+          methodCall(
+              createdAt: tparams.createdAt,
+              name: tparams.name,
+              avatar: tparams.avatar),
+          completes);
+    });
+  });
 }
