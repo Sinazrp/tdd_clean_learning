a4454497721d6e8ac0b8012277687d7e1057e6e7
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-18 23:04:44 +0330

    Update auth_remote_datasource.dart

--- lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:tdd_clean_learning/features/auth/data/models/user_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class AuthRemoteDataSource {
   Future<void> createUser(
@@ -7,4 +8,23 @@ abstract class AuthRemoteDataSource {
       required String avatar});
 
   Future<List<UserModel>> getUsers();
+}
+
+class AuthRemoteDataSourceImple implements AuthRemoteDataSource {
+  final http.Client _client;
+
+  AuthRemoteDataSourceImple(this._client);
+
+  @override
+  Future<void> createUser(
+      {required String createdAt,
+      required String name,
+      required String avatar}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<UserModel>> getUsers() {
+    throw UnimplementedError();
+  }
 }
