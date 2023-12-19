3f988d008e05b37f74a53cda33cc709e9ddf8bc5
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-19 20:12:26 +0330

    Update auth_remote_datasource_test.dart

--- test/features/auth/data/datasources/remote/auth_remote_datasource_test.dart
+++ test/features/auth/data/datasources/remote/auth_remote_datasource_test.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
+import 'package:tdd_clean_learning/core/utils/consts.dart';
 import 'package:tdd_clean_learning/features/auth/data/datasources/remote/auth_remote_datasource.dart';
 import 'package:tdd_clean_learning/features/auth/data/models/user_model.dart';
 
@@ -11,6 +14,7 @@ void main() {
   UserModel tparams = const UserModel.empty();
   late http.Client client;
   late AuthRemoteDataSource remoteDataSource;
+  registerFallbackValue(Uri());
   setUp(() {
     client = MockHttpClient();
     remoteDataSource = AuthRemoteDataSourceImple(client);
@@ -19,7 +23,7 @@ void main() {
     test('should complete successfully', () async {
       when(() => client.post(any(), body: any(named: 'body'))).thenAnswer(
           (_) async => http.Response('Usser Created succesfully', 201));
-      final methodCall = await remoteDataSource.createUser;
+      final methodCall = remoteDataSource.createUser;
 
       expect(
           methodCall(
@@ -27,6 +31,16 @@ void main() {
               name: tparams.name,
               avatar: tparams.avatar),
           completes);
+      verify(() => client.post(Uri.parse("$baseUrl/users"),
+          body: jsonEncode({
+            'avatar': tparams.avatar,
+            'createdAt': tparams.createdAt,
+            'name': tparams.name,
+          }))).called(1);
+    });
+    test('should throw api exceptiobn', () {
+      when(() => client.post(any(), body: any(named: 'body')))
+          .thenAnswer((_) async => http.Response('faild to created', 500));
     });
   });
 }
