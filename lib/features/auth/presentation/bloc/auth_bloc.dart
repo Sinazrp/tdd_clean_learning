8411d5e865eaa71af37814a63329bd08910c4fb2
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-23 21:17:19 +0330

    Update auth_bloc.dart

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,11 +1,26 @@
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_clean_learning/features/auth/domain/usecases/create_user.dart';
+import 'package:tdd_clean_learning/features/auth/domain/usecases/get_user.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {});
+  AuthBloc({required CreateUser createUser, required GetUser getUser})
+      : _createUser = createUser,
+        _getUser = getUser,
+        super(AuthInitial()) {
+    on<CreateUserEvent>(_createUserEvent);
+    on<GetUserEvent>(_getUserEvent);
   }
+  final CreateUser _createUser;
+  final GetUser _getUser;
+
+  FutureOr<void> _createUserEvent(
+      CreateUserEvent event, Emitter<AuthState> emit) {}
+
+  FutureOr<void> _getUserEvent(GetUserEvent event, Emitter<AuthState> emit) {}
 }
