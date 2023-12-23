33c0e687dc4bc6f394d18c6d4ef31a54aaeb9a4f
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-23 21:34:43 +0330

    Update auth_state.dart

--- lib/features/auth/presentation/bloc/auth_state.dart
+++ lib/features/auth/presentation/bloc/auth_state.dart
@@ -1,9 +1,33 @@
 part of 'auth_bloc.dart';
 
 abstract class AuthState extends Equatable {
-  const AuthState();  
+  const AuthState();
 
   @override
   List<Object> get props => [];
 }
-class AuthInitial extends AuthState {}
+
+class AuthInitial extends AuthState {
+  const AuthInitial();
+}
+
+class CreatingUser extends AuthState {}
+
+class GettingUser extends AuthState {}
+
+class UserCreated extends AuthState {}
+
+class UsersLoaded extends AuthState {
+  const UsersLoaded(this.users);
+  final List<UserModel> users;
+  @override
+  //! it is very important to do this on lists
+  List<Object> get props => users.map((e) => e.id).toList();
+}
+
+class AuthError extends AuthState {
+  final String message;
+  const AuthError(this.message);
+  @override
+  List<String> get props => [message];
+}
