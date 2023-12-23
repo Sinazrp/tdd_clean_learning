a2171ce42fd4d04643c7e3088732019f7fc1c078
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-24 01:32:26 +0330

    Update auth_bloc.dart

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_clean_learning/features/auth/domain/entities/user.dart';
 import 'package:tdd_clean_learning/features/auth/domain/usecases/create_user.dart';
 import 'package:tdd_clean_learning/features/auth/domain/usecases/get_user.dart';
 
@@ -12,7 +13,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc({required CreateUser createUser, required GetUser getUser})
       : _createUser = createUser,
         _getUser = getUser,
-        super(AuthInitial()) {
+        super(const AuthInitial()) {
     on<CreateUserEvent>(_createUserEvent);
     on<GetUserEvent>(_getUserEvent);
   }
@@ -20,7 +21,19 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final GetUser _getUser;
 
   FutureOr<void> _createUserEvent(
-      CreateUserEvent event, Emitter<AuthState> emit) {}
+      CreateUserEvent event, Emitter<AuthState> emit) async {
+    emit(const CreatingUserState());
+    final result = await _createUser(CreateUserParams(
+        createdAt: event.createdAt, name: event.name, avatar: event.avatar));
+    result.fold((failure) => emit(AuthErrorState(failure.getMessage())),
+        (_) => emit(UserCreatedState()));
+  }
 
-  FutureOr<void> _getUserEvent(GetUserEvent event, Emitter<AuthState> emit) {}
+  FutureOr<void> _getUserEvent(
+      GetUserEvent event, Emitter<AuthState> emit) async {
+    emit(GettingUserState());
+    final result = await _getUser();
+    result.fold((failure) => emit(AuthErrorState(failure.message)),
+        (usersList) => emit(UsersLoadedState(usersList)));
+  }
 }
