2c889a3f45a7ebb0b961f7e78dbc6d1376e1081f
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2023-12-23 21:04:05 +0330

    Update auth_event.dart

--- lib/features/auth/presentation/bloc/auth_event.dart
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -2,7 +2,16 @@ part of 'auth_bloc.dart';
 
 abstract class AuthEvent extends Equatable {
   const AuthEvent();
+}
+
+class CreateUserEvent extends AuthEvent {
+  final String cretedAt;
+  final String name;
+  final String avatar;
+
+  const CreateUserEvent(
+      {required this.cretedAt, required this.name, required this.avatar});
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [cretedAt, name, avatar];
 }
